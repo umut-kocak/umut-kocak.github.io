500384d937aa1ea101dfac39295369605bccf04f
umut-kocak <umut.kocak@gmail.com>
2024-09-28 23:49:34 +0200

    Add google analytics.

---
+++ js/analytics.js
@@ -0,0 +1,12 @@
+// analytics.js
+(function() {
+  var gaScript = document.createElement('script');
+  gaScript.async = true;
+  gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-82Y5PB1226"';
+  document.head.appendChild(gaScript);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', 'G-82Y5PB1226');
+})();
